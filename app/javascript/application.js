b10bab8f56f13c891f5e3434f3a274f796a5c60d
PhonePixelGhost <phonepixelghost@hotmail.com>
2024-10-10 21:59:47 +0700

    todos homework

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+function showIncomplete() {
+    document.getElementById('incomplete-tasks').style.display = 'block';
+    document.getElementById('complete-tasks').style.display = 'none';
+  }
+  
+  function showComplete() {
+    document.getElementById('incomplete-tasks').style.display = 'none';
+    document.getElementById('complete-tasks').style.display = 'block';
+  }
+  
